774c537fbc25488d83659ba090c5158273849893
Ilya Chalyshkou <great.mars15@gmail.com>
2021-07-22 13:51:56 +0300

    update: have updated game import logic.

--- main.js
+++ main.js
@@ -2,4 +2,6 @@ import { View } from "./view.js";
 import { Model } from "./model.js";
 import { Controller } from "./controller.js";
 
-new Controller(new View(), new Model()).start("container");
+export function importGame(containerId) {
+	new Controller(new View(), new Model()).start(containerId);
+}
